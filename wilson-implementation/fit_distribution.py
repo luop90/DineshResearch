35307f821db5bb360526714a757e3cfeb078c437
Lucas Henke <lhenke@gc228-20.mines.edu>
2021-02-10 15:48:06 -0700

    Added more

--- wilson-implementation/fit_distribution.py
+++ wilson-implementation/fit_distribution.py
@@ -8,16 +8,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from timing_object import TimingJob
 
+import scipy.stats
+from sklearn.neighbors import KernelDensity
+from numpy import asarray
+from numpy import exp
+
 # List all the TimingJobs in this array 
 all_graphs = [
-    TimingJob("clique", lambda n, params: nx.complete_graph(n)),
-    # TimingJob("chain", lambda n, params: nx.path_graph(n)),
-    # TimingJob("lollipop", lambda n, params: nx.lollipop_graph(round(n/2), round(n/2))),
-    TimingJob("barabasi_albert_n-1", lambda n, params: nx.barabasi_albert_graph(n, n-1)),
-    TimingJob("barabasi_albert_.75", lambda n, params: nx.barabasi_albert_graph(n, round(0.75*n))),
-    TimingJob("barabasi_albert_0.5", lambda n, params: nx.barabasi_albert_graph(n, round(0.5*n))),
-    TimingJob("barabasi_albert_0.25", lambda n, params: nx.barabasi_albert_graph(n, round(0.25*n))),
-    TimingJob("barabasi_albert_2", lambda n, params: nx.barabasi_albert_graph(n, 2))
+    #TimingJob("clique", lambda n, params: nx.complete_graph(n)),
+    #TimingJob("chain", lambda n, params: nx.path_graph(n)),
+    TimingJob("lollipop", lambda n, params: nx.lollipop_graph(round(n/2), round(n/2))),
 ]
 
 # string-ify all the names, comma-separated
@@ -26,6 +26,54 @@ all_names = ','.join([g.name for g in all_graphs])
 csv_output = f"n, {all_names}\n" 
 print("Beginning the timing")
 print(csv_output)
+
+def fit_and_plot_distribution2(name, distribution, data):
+    model = KernelDensity(bandwidth=2, kernel='gaussian')
+    data = asarray(data)
+    data = data.reshape((len(data),  1))
+    model.fit(data)
+
+    # sample probabilities for a range of outcomes
+    values = asarray([value for value in range(1, 60)])
+    values = values.reshape((len(values), 1))
+    probabilities = model.score_samples(values)
+    probabilities = exp(probabilities)
+
+    # plot the histogram and pdf
+    # pyplot.hist(sample, bins=50, density=True)
+    # pyplot.plot(values[:], probabilities)
+    # pyplot.show()
+
+
+    plt.figure()
+    plt.hist(data, density=True)
+    plt.plot(values[:], probabilities, 'r--')
+    # plt.plot(x, fitted_data, 'r--')
+
+    plt.title(f'{name} Distribution')
+    plt.xlabel('num calls to RandomSuccessor()')
+    plt.ylabel('count')
+
+    plt.savefig(f'aahistogram_{name}_{len(data)}_lollipop.png')
+    plt.close()
+
+# Fits the scipy distribution to the data, and plots the result
+def fit_and_plot_distribution(name, distribution, data):
+    mean, variance = distribution.fit(data)
+
+    x = np.linspace(min(data)-5, max(data)+5, 1000)
+    fitted_data = distribution.pdf(x, mean, variance)
+
+    plt.figure()
+    plt.hist(data, density=True)
+    plt.plot(x, fitted_data, 'r--')
+
+    plt.title(f'{name} Distribution')
+    plt.xlabel('num calls to RandomSuccessor()')
+    plt.ylabel('count')
+
+    plt.savefig(f'aahistogram_{name}_{len(data)}_lollipop.png')
+    plt.close()
 
 # Plots a histogram of the work values, and stores base data
 # data = list of scalars
@@ -46,17 +94,18 @@ def plot_histogram(data, graph_type, num_vertices):
 
     plt.savefig(f'histogram_{graph_type}_{num_vertices}.png')
     
-    with open(f'data_{graph_type}_{num_vertices}.txt', 'w') as f:
+    with open(f'data_{graph_type}_{num_vertices}_1000_points.txt', 'w') as f:
         f.writelines("%s\n" % point for point in data)
     plt.close()
 
-
+global_time_array = []
 # Samples the "run-time" distribution of Wilson's algorithm on this graph
 # graph = NetworkX graph object
 # graph_name = name of the gragh, e.g. "complete"
 # n = number of vertices
 def time_wilsons_algorithm(graph, graph_name, n):
     time_array = []
+    global global_time_array
 
     # we're technically sampling a distribution, so >30 for the central-limit theorem to take effect
     # it's looking like a *super* skewed exponential distribution, so lets do 60 samples to get better
@@ -67,11 +116,12 @@ def time_wilsons_algorithm(graph, graph_name, n):
         (_, num_calls) = RandomTreeWithRoot(root, graph)
         time_array.append(num_calls)
     
+    global_time_array = time_array
     plot_histogram(time_array, graph_name, n)
     return mean(time_array)
 
 
-for i in range(1, 101): # 100 times, each one get's larger
+for i in range(1, 2): # 100 times, each one get's larger
     multiplier = 100
     n = multiplier * i # number of nodes = 100, 200, etc.
 
